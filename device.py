d3c51ff0b6a048dc49f7dfa0d4c1c7559a76bbab
azbairos <azbairos@gmail.com>
2022-01-02 23:05:53 -0500

    made a fake keyboard for fleshy

--- device.py
+++ device.py
@@ -1,3 +1,5 @@
+import keyboard
+
 class Messages(object):
 
     def __init__(self):
@@ -26,57 +28,65 @@ class Device(object):
         self.input_buffer = ""
 
     def get_keyboard_input(self):
-        return {"a": True}
+        # @TODO THIS NEEDS TO CHANGE FOR lora controller implementation
+
+        all_possible = {
+            "a": False,
+            "b": False,
+            "c": False,
+            "d": False,
+            "e": False,
+            "f": False,
+            "g": False,
+            "h": False,
+            "i": False,
+            "j": False,
+            "k": False,
+            "l": False,
+            "m": False,
+            "n": False,
+            "o": False,
+            "p": False,
+            "q": False,
+            "r": False,
+            "s": False,
+            "t": False,
+            "u": False,
+            "v": False,
+            "w": False,
+            "x": False,
+            "y": False,
+            "z": False,
+            "0": False,
+            "1": False,
+            "2": False,
+            "3": False,
+            "4": False,
+            "5": False,
+            "6": False,
+            "7": False,
+            "8": False,
+            "9": False,
+            "enter":False,
+            "right shift":False, # THIS WILL BE FUNCTION
+            "space":False,
+            "backspace":False
+        }
+        pressing = keyboard.read_key()
+        if pressing in all_possible.keys():
+            all_possible[pressing] = True
+        return all_possible
 
     def get_message(self):
         data_in = self.lora.read_from_device()
         if data_in is not None:
             self.messages.add(data_in)
 
+    def setup_keyboard(self):
+        pass
+
+
+
 
 if __name__ == "__main__":
-    data = {
-        1: "a",
-        2: "b"
-    }
-    data2 = {
-        1: "c",
-        2: "d"
-    }
-    data3 = {
-        1: "c",
-        2: "z"
-    }
-    data4 = {
-        1: "z",
-        2: "d"
-    }
-    data5 = {
-        1: "f",
-        2: "d"
-    }
-    data6 = {
-        1: "c",
-        2: "f"
-    }
-    data7 = {
-        1: "g",
-        2: "d"
-    }
-    data8 = {
-        1: "c",
-        2: "g"
-    }
-    data9 = {
-        1: "c",
-        2: "h"
-    }
-    data10 = {
-        1: "i",
-        2: "h"
-    }
-    data11 = {
-        1: "o",
-        2: "d"
-    }
-    m = Messages()
+    pass
