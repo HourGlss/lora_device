aa3937dc16cfe4ed60f4943277938b71f04c8a29
fleshmanj <fleshmanjoshuac@hotmail.com>
2022-01-09 10:12:37 -0600

    Changes by azbai

--- pico code/lib/states.py
+++ pico code/lib/states.py
@@ -52,6 +52,7 @@ class AbstractState(object):
 
 
 class ComposeMenu(AbstractState):
+
     def __init__(self, d):
         super().__init__(d)
         self.device.next_cursor_row = 0
@@ -78,9 +79,6 @@ class ComposeMenu(AbstractState):
                                                           self.device.next_input_buffer,
                                                           self.device.current_screen[60:])
             self.device.input_buffer = self.device.next_input_buffer
-
-        else:
-            pass
 
     def use_keyboard_input(self, kb):
         if kb['enter']:
@@ -114,8 +112,10 @@ class ComposeMenu(AbstractState):
     def on_enter(self):
         if self.device.cursor_row == 3 and self.device.cursor_col == 0:
             self.nextState = MainMenu(self.device)
+
         elif self.device.cursor_row == 3 and self.device.cursor_col == 2:
             self.nextState = SendMenu(self.device)
+
         elif self.device.cursor_row == 0 and self.device.cursor_col <= 5:
             if self.device.input_buffer is not None:
                 self.nextState = SendingMenu(self.device)
@@ -145,50 +145,65 @@ class ComposeMenu(AbstractState):
             elif self.device.next_cursor_row > 0:
                 self.device.next_cursor_col = self.device.lcd_width - 1
                 self.device.next_cursor_row = self.device.cursor_row - 1
-            else:
-                pass
-
-    def get_next_state(self):
-        if self.nextState is None:
-            return self
-        else:
-            return self.nextState
 
 
 class SendingMenu(AbstractState):
 
     def __init__(self, d):
         super().__init__(d)
+        self.addr_to_use = None
+        self.device.next_cursor_row = 0
+        self.device.next_cursor_col = 0
+        self.device.function_toggle = False
+        self.device.toggle_lcd_event_flag()
 
-    def get_next_state(self):
-        pass
+    def initial(self):
+        self.device.initial_cursor_row = 0
+        self.device.initial_cursor_col = 0
+        self.device.toggle_lcd_event_flag()
+        self.device.input_buffer = ""
+        self.device.next_input_buffer = ""
+        menu = ("Sending message", "", "", "")
+        self.device.next_screen = ""
+        for item in menu:
+            self.device.next_screen += "{:<20}".format(item)
+
+        if self.device.lora.send(str(self.device.data_to_send["data"]), self.device.data_to_send["address"]):
+            self.last_sent = self.device.data_to_send
+            self.nextState = SendSuccessful(self.device)
+            self.device.data_to_send = {}
+        else:
+            self.nextState = SendingMenu(self.device)
+
+
+class SendSuccessful(AbstractState):
+
+    def __init__(self, d):
+        super().__init__(d)
+        self.addr_to_use = None
+        self.device.next_cursor_row = 0
+        self.device.next_cursor_col = 0
+        self.device.function_toggle = False
+        self.device.toggle_lcd_event_flag()
+
+    def initial(self):
+        self.device.initial_cursor_row = 0
+        self.device.initial_cursor_col = 0
+        self.device.toggle_lcd_event_flag()
+        self.device.input_buffer = ""
+        self.device.next_input_buffer = ""
+        menu = ("Send Successful", "", "", "")
+        self.device.next_screen = ""
+        for item in menu:
+            self.device.next_screen += "{:<20}".format(item)
 
     def use_keyboard_input(self, kb):
         if kb['enter']:
             self.on_enter()
-        if kb['left']:
-            self.on_left()
-        if kb['right']:
-            self.on_right()
-        if kb['up']:
-            self.on_up()
-        if kb['down']:
-            self.on_down()
-        pass
-
-    def print_input_buffer(self):
-        pass
-
-    def update_screen(self):
-        pass
+            return
 
     def on_enter(self):
-        pass
-
-    def write_char(self, c):
-        pass
-
-    def delete(self):
+        self.nextState = MainMenu(self.device)
         pass
 
 
@@ -196,74 +211,312 @@ class SettingsMenu(AbstractState):
 
     def __init__(self, d):
         super().__init__(d)
+        self.addr_to_use = None
+        self.device.next_cursor_row = 0
+        self.device.next_cursor_col = 0
+        self.device.toggle_lcd_event_flag()
 
-    def get_next_state(self):
-        pass
+    def initial(self):
+        self.device.toggle_lcd_event_flag()
+        self.device.next_screen = ""
+        menu = ("* Set addr (0-65535)", "* Set ntwk (0-16)", "", "M")
+        for item in menu:
+            self.device.next_screen += "{:<20}".format(item)
 
     def use_keyboard_input(self, kb):
         if kb['enter']:
             self.on_enter()
-        if kb['left']:
-            self.on_left()
-        if kb['right']:
-            self.on_right()
-        if kb['up']:
-            self.on_up()
-        if kb['down']:
+            return
+        if kb['s']:
             self.on_down()
-        pass
-
-    def print_input_buffer(self):
-        pass
-
-    def update_screen(self):
-        pass
+            return
+        if kb['w']:
+            self.on_up()
+            return
 
     def on_enter(self):
-        pass
+        if self.device.cursor_row == 0:
+            self.nextState = SetAddress(self.device)
+        if self.device.cursor_row == 1:
+            self.nextState = SetNetworkid(self.device)
+        if self.device.cursor_row == 3:
+            self.nextState = MainMenu(self.device)
+
+    def on_up(self):
+        self.device.toggle_lcd_event_flag()
+        if self.device.cursor_row > 0:
+            self.device.next_cursor_row = self.device.cursor_row - 1
+            if self.device.next_cursor_row == 2:
+                self.device.next_cursor_row -= 1
+
+    def on_down(self):
+        self.device.toggle_lcd_event_flag()
+        if self.device.cursor_row < self.device.lcd_height - 1:
+            self.device.next_cursor_row = self.device.cursor_row + 1
+            if self.device.next_cursor_row == 2:
+                self.device.next_cursor_row += 1
+        self.device.next_cursor_col = 0
+
+
+class SetAddress(AbstractState):
+
+    def __init__(self, d):
+        super().__init__(d)
+        self.device.function_toggle = False
+        self.device.next_cursor_row = 0
+        self.device.next_cursor_col = 0
+        self.device.toggle_lcd_event_flag()
+        self.device.input_buffer = ""
+        self.device.next_input_buffer = ""
+
+    def initial(self):
+        self.device.next_cursor_row = 1
+        self.device.next_cursor_col = 0
+        self.device.toggle_lcd_event_flag()
+        self.device.next_screen = ""
+        menu = ("Set addr (0-65535)", "", "enter", "M")
+        for item in menu:
+            self.device.next_screen += "{:<20}".format(item)
+
+    def use_keyboard_input(self, kb):
+        if kb['enter']:
+            self.on_enter()
+            return
+        if kb['right shift']:
+            self.device.function_toggle = not self.device.function_toggle
+            return
+        if self.device.function_toggle:
+            if kb['s']:
+                self.on_down()
+                return
+            if kb['w']:
+                self.on_up()
+                return
+        if kb['backspace']:
+            self.delete()
+            return
+
+        for k in kb.keys():
+            if kb[k]:
+                self.write_char(k)
+                break
+
+    def screen(self):
+        if self.device.input_buffer != self.device.next_input_buffer:
+            self.device.next_screen = "{}{:<5}{}".format(self.device.current_screen[0:20],
+                                                         self.device.next_input_buffer,
+                                                         self.device.current_screen[25:])
+            print(self.device.next_screen)
+            self.device.input_buffer = self.device.next_input_buffer
+
+    def on_enter(self):
+        # checks if the the cursor is at 0,0
+        if self.device.next_cursor_row == 2 and self.device.next_cursor_col <= 5:
+            # transitions to the main_menu state
+            self.nextState = MainMenu(self.device)
+            # saves the address to the data_to_send
+            self.device.data_to_send["address_to_use"] = int(self.device.input_buffer)
+            self.device.data_to_send = {}
 
     def write_char(self, c):
-        pass
+        addr = None
+        if c in [str(e) for e in range(0, 10)]:
+            self.device.next_input_buffer += c
+            try:
+                addr = int(self.device.next_input_buffer)
+                if addr > 65535:
+                    raise ValueError
+                self.device.next_cursor_col = self.device.cursor_col + 1
+                self.device.toggle_lcd_event_flag()
+            except:
+                self.device.next_input_buffer = self.device.next_input_buffer[:-1]
+        if addr is not None:
+            self.addr_to_use = addr
 
     def delete(self):
-        pass
+        if self.device.cursor_row == 1:
+            if len(self.device.next_input_buffer) > 0:
+                self.device.next_input_buffer = self.device.next_input_buffer[:-1]
+                self.device.toggle_lcd_event_flag()
+                self.device.next_cursor_col = self.device.cursor_col - 1
+
+    def on_up(self):
+        self.device.toggle_lcd_event_flag()
+        if self.device.cursor_row > 0:
+            self.device.next_cursor_row = self.device.cursor_row - 1
+            if self.device.next_cursor_row == 1:
+                self.device.next_cursor_col = len(self.device.input_buffer)
+            else:
+                self.device.next_cursor_col = 0
+
+    def on_down(self):
+        self.device.toggle_lcd_event_flag()
+        if self.device.cursor_row < self.device.lcd_height - 1:
+            self.device.next_cursor_row = self.device.cursor_row + 1
+            if self.device.next_cursor_row == 1:
+                self.device.next_cursor_col = len(self.device.input_buffer)
+            else:
+                self.device.next_cursor_col = 0
+
+
+class SetNetworkid(AbstractState):
+
+    def __init__(self, d):
+        super().__init__(d)
+        self.device.function_toggle = False
+        self.device.next_cursor_row = 0
+        self.device.next_cursor_col = 0
+        self.device.toggle_lcd_event_flag()
+        self.device.input_buffer = ""
+        self.device.next_input_buffer = ""
+
+    def initial(self):
+        self.device.next_cursor_row = 1
+        self.device.next_cursor_col = 0
+        self.device.toggle_lcd_event_flag()
+        self.device.next_screen = ""
+        menu = ("* Set ntwk (0-16)", "", "enter", "M")
+        for item in menu:
+            self.device.next_screen += "{:<20}".format(item)
+
+    def use_keyboard_input(self, kb):
+        if kb['enter']:
+            self.on_enter()
+            return
+        if kb['right shift']:
+            self.device.function_toggle = not self.device.function_toggle
+            return
+        if self.device.function_toggle:
+            if kb['s']:
+                self.on_down()
+                return
+            if kb['w']:
+                self.on_up()
+                return
+        if kb['backspace']:
+            self.delete()
+            return
+
+        for k in kb.keys():
+            if kb[k]:
+                self.write_char(k)
+                break
+
+    def screen(self):
+        if self.device.input_buffer != self.device.next_input_buffer:
+            self.device.next_screen = "{}{:<2}{}".format(self.device.current_screen[0:20],
+                                                         self.device.next_input_buffer,
+                                                         self.device.current_screen[22:])
+            self.device.input_buffer = self.device.next_input_buffer
+
+    def on_enter(self):
+        if self.device.next_cursor_row == 2 and self.device.next_cursor_col <= 5:
+            self.nextState = MainMenu(self.device)
+            self.device.data_to_send["networkid_to_use"] = int(self.device.input_buffer)
+            self.device.data_to_send = {}
+
+    def write_char(self, c):
+        addr = None
+        if c in [str(e) for e in range(0, 10)]:
+            self.device.next_input_buffer += c
+            try:
+                addr = int(self.device.next_input_buffer)
+                if addr > 16:
+                    raise ValueError
+                self.device.next_cursor_col = self.device.cursor_col + 1
+                self.device.toggle_lcd_event_flag()
+            except:
+                self.device.next_input_buffer = self.device.next_input_buffer[:-1]
+        if addr is not None:
+            self.addr_to_use = addr
+
+    def delete(self):
+        if self.device.cursor_row == 1:
+            if len(self.device.next_input_buffer) > 0:
+                self.device.next_input_buffer = self.device.next_input_buffer[:-1]
+                self.device.toggle_lcd_event_flag()
+                self.device.next_cursor_col = self.device.cursor_col - 1
+
+    def on_up(self):
+        self.device.toggle_lcd_event_flag()
+        if self.device.cursor_row > 0:
+            self.device.next_cursor_row = self.device.cursor_row - 1
+            if self.device.next_cursor_row == 1:
+                self.device.next_cursor_col = len(self.device.input_buffer)
+            else:
+                self.device.next_cursor_col = 0
+
+    def on_down(self):
+        self.device.toggle_lcd_event_flag()
+        if self.device.cursor_row < self.device.lcd_height - 1:
+            self.device.next_cursor_row = self.device.cursor_row + 1
+            if self.device.next_cursor_row == 1:
+                self.device.next_cursor_col = len(self.device.input_buffer)
+            else:
+                self.device.next_cursor_col = 0
 
 
 class ReceivedMenu(AbstractState):
 
     def __init__(self, d):
         super().__init__(d)
+        self.addr_to_use = None
+        self.last_sender = None
+        self.sender = None
+        self.message = ""
+        self.current_message = 0
+        self.device.next_cursor_row = 0
+        self.device.next_cursor_col = 0
+        self.device.function_toggle = False
 
-    def get_next_state(self):
-        pass
+    def initial(self):
+        self.device.initial_cursor_row = 3
+        self.device.initial_cursor_col = 0
+        self.device.next_cursor_row = self.device.initial_cursor_row
+        self.device.next_cursor_col = self.device.initial_cursor_col
+        self.device.toggle_lcd_event_flag()
+        self.device.input_buffer = ""
+        self.device.next_input_buffer = ""
+        menu = ("Message from:", "", "", "M")
+        self.device.next_screen = ""
+        for item in menu:
+            self.device.next_screen += "{:<20}".format(item)
+
+    def screen(self):
+        if self.sender != self.last_sender:
+            self.device.next_screen = "{}{:<6}{:<40}{:<20}".format(self.device.current_screen[0:14], str(self.sender),
+                                                                   self.message,
+                                                                   self.device.current_screen[60:])
+            self.device.last_sender = self.sender
 
     def use_keyboard_input(self, kb):
+        if kb['s']:
+            self.next_message()
+            return
+        if kb['w']:
+            self.last_message()
+            return
         if kb['enter']:
             self.on_enter()
-        if kb['left']:
-            self.on_left()
-        if kb['right']:
-            self.on_right()
-        if kb['up']:
-            self.on_up()
-        if kb['down']:
-            self.on_down()
-        pass
-
-    def print_input_buffer(self):
-        pass
-
-    def update_screen(self):
-        pass
+            return
 
     def on_enter(self):
-        pass
+        if self.device.next_cursor_row == 3:
+            self.nextState = MainMenu(self.device)
 
-    def write_char(self, c):
-        pass
+    def next_message(self):
+        self.device.toggle_lcd_event_flag()
+        if self.current_message < len(self.device.messages) - 1:
+            self.current_message += 1
+        self.sender = self.device.messages[self.current_message]['address']
+        self.message = self.device.messages[self.current_message]['data']
 
-    def delete(self):
-        pass
+    def last_message(self):
+        self.device.toggle_lcd_event_flag()
+        if self.current_message > 0:
+            self.current_message -= 1
+        self.sender = self.device.messages[self.current_message]['address']
+        self.message = self.device.messages[self.current_message]['data']
 
 
 class SendMenu(AbstractState):
@@ -378,9 +631,6 @@ class SendMenu(AbstractState):
 
 class MainMenu(AbstractState):
 
-    def initial(self):
-        pass
-
     def __init__(self, d):
         super().__init__(d)
         self.device.function_toggle = False
@@ -407,27 +657,14 @@ class MainMenu(AbstractState):
             self.device.next_screen += "{:<20}".format(item)
 
     def on_enter(self):
-        # checks if the the cursor is at 0,0
         if self.device.cursor_row == 0:
-            # transitions to the send_new state
             self.nextState = SendMenu(self.device)
-        # checks if the the cursor is at 1,0
         if self.device.cursor_row == 1:
-            # transitions to the received_menu state
             self.nextState = ReceivedMenu(self.device)
-        # checks if the the cursor is at 2,0
         if self.device.cursor_row == 2:
-            # transitions to the received_menu state
             self.nextState = SettingsMenu(self.device)
-
-    def write_char(self, c):
-        pass
-
-    def delete(self):
-        pass
 
     def on_down(self):
         if self.device.cursor_row < self.lowest_row:
-            # increments the row
             self.device.next_cursor_row = self.device.cursor_row + 1
             self.device.toggle_lcd_event_flag()
